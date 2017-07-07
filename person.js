9f4cb10a6427771d2c723b7e5315b69168c4c2e4
Gabor Abraham <godgab@gmail.com>
2017-07-07 22:32:20 +0200

    Modify code to support AMD loading.

--- person.js
+++ person.js
@@ -1,35 +1,59 @@
-class Person {
-	constructor(name, age) {
-		this.name = name;
-		this.age = age;
+require.config({
+	paths: {
+		jquery: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min",
+		prompt: "prompt",
 	}
-}
+});
 
-class PersonPrompt extends Prompt {
-	constructor(params) {
-		super({
-			callback: function(message, def, type){ return prompt(message, def); },
-
-			prompts: [
-				{
-					message: "Your name:",
-					type: String,
-					initial: "Name...",
-					regex: new RegExp(/(\S+)/),
-				},
-				{
-					message: "Age:",
-					type: Number,
-					regex: new RegExp(/(\d+)/),
-					validator: (function(value) { return (value >= 1 && value < 120); }),
-				},
-			],
-
-			create: Person,
-		});
+require(["jquery", "prompt"], function($, Prompt)
+{
+	class Person {
+		constructor(name, age) {
+			this.name = name;
+			this.age = age;
+		}
 	}
 
-	create() {
-		return super.create();
+	class PersonPrompt extends Prompt {
+		constructor(params) {
+			super({
+				callback: function(message, def, type){ return prompt(message, def); },
+
+				prompts: [
+					{
+						message: "Your name:",
+						type: String,
+						initial: "Name...",
+						regex: new RegExp(/(\S+)/),
+					},
+					{
+						message: "Age:",
+						type: Number,
+						regex: new RegExp(/(\d+)/),
+						validator: (function(value) { return (value >= 1 && value < 120); }),
+					},
+				],
+
+				create: Person,
+			});
+		}
+
+		create() {
+			return super.create();
+		}
 	}
-}
+
+	$("#button").click(function()
+	{
+		var personPrompt = new PersonPrompt();
+		var person = personPrompt.create();
+		var node = document.createElement("p");
+		node.innerHTML += "Name: " + person.name + "</br>";
+		node.innerHTML += "Age: " + person.age;
+		document.body.appendChild(node);
+	});
+
+
+
+
+});
